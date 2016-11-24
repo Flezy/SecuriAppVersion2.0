ac4046122ab750a19db6c43081af0fffdd03d47e
Iamtazy <szabo94@windowslive.com>
2016-11-24 22:09:31 +0100

    added models

--- Backend/SecuriMaster/SecuriM/models.py
+++ Backend/SecuriMaster/SecuriM/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+class Guard(models.Model):
+    
+    id = models.IntegerField
+    name = models.CharField
+    
+    class Meta:
+        db_table = "guard"
+        
+
+class Post(models.Model):
+    
+    id = models.IntegerField
+    name = models.CharField
+    
+    class Meta:
+        db_table = "post"
+        
+
+class User(models.Model):
+    
+    id = models.IntegerField
+    username = models.CharField
+    password = models.CharField
+    
+    class Meta:
+        db_table = "User"
+        
+        
+class Points(models.Model):
+    
+    id = models.IntegerField
+    nameOfRating = models.CharField
+    critical = models.BooleanField
+    
+    class Meta:
+        db_table = "points"
+        
+class CheckDetails(models.Model):
+    
+    id = models.IntegerField
+    date = models.DateTimeField
+    
+    class Meta:
+        db_table = "check_details"
